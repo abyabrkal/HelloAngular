784bdf0cd429627fdb7b38bfd6077e02d680f7c9
Aby Abraham <abyabrkal@gmail.com>
2018-05-20 18:55:36 +0400

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'IronMan' },
+    { id: 12, name: 'Captain America' },
+    { id: 13, name: 'Thor' },
+    { id: 14, name: 'Hulk' },
+    { id: 15, name: 'StarLord' },
+    { id: 16, name: 'Black Panther' },
+    { id: 17, name: 'Groot' },
+    { id: 18, name: 'Marvel' },
+    { id: 19, name: 'WhiteWolf' },
+    { id: 20, name: 'Vision'}
+];
